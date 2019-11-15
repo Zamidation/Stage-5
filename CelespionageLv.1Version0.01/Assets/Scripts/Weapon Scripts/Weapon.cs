0d4014bc7e474f02228379e4d6ac83e0e4b077ac
CorreianAngel <43967709+CorreianAngel@users.noreply.github.com>
2019-11-14 18:02:40 -0800

    Added import package.

---
+++ CelespionageLv.1Version0.01/Assets/Scripts/Weapon Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject bullet;
+    
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.GetComponent<Rigidbody2D>().AddForce
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        
+
+    }
+}
