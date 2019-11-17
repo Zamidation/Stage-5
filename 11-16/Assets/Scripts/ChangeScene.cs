7dfe216e5c3e48ec1140c379c7bf9b371a8a4e60
Zamidation <48175990+Zamidation@users.noreply.github.com>
2019-11-16 19:15:35 -0800

    montana add stuff

---
+++ 11-16/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int index;
+    public string levelname;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) // trigger whene the collider box collides with a game object with the player Tag
+        {
+            SceneManager.LoadScene(index); // sets an index for how many scene  it can hold
+            SceneManager.LoadScene("FirstScene");   // load a the following scene name 
+                
+        }   
+    }
+}
---
+++ CelespionageLv.1Version0.01/Assets/Personal Folders/Montana/ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public SceneManager currentscene;
+    public SceneManager nextscene;
+
+    //private bool 
+
+    public KeyCode EnterDoorKey = KeyCode.T;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+
+}
