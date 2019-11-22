1bd963425811240b6658f56a4b6b6168a20a34a9
Zion Jones <Joneszion412@gmail.com>
2019-11-21 19:38:35 -0800

    Added tyler and Alexs package and edited ps power ups

---
+++ CelespionageLv.1Version0.01/Assets/ZionScripts/PowerUp Scripts/PowerUpDoubleJump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpDoubleJump : MonoBehaviour   //power up actions
+{
+    private PlayerMovement playerMovement; 
+
+    public void DoubleJumpStartAction(){
+        playerMovement.JumpNum += 2;
+    }
+
+    public void DoubleJumpEndAction(){
+        playerMovement.JumpNum = playerMovement.defaultJump;
+    }
+
+}
---
+++ CelespionageLv.1Version0.01/Assets/ZionScripts/Weapon Scripts/Bullet.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speedmultiplier = 5.0f;
+
+    private bool isMovingRight = true;
+    private Rigidbody2D rb;
+
+    public int damage = 5;
+
+    private void Awake()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        if (player.transform.localScale.x > 0)
+        {
+            isMovingRight = true;            
+        }
+
+        else if (player.transform.localScale.x < 0)
+        {
+            isMovingRight = false;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Destroy(gameObject, 5.0f);
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isMovingRight)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speedmultiplier);
+        }
+
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speedmultiplier);
+        }
+
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+
+    }
+
+}
