e2b71689724cff614cd350722d68e5aec886873b
Zamidation <48175990+Zamidation@users.noreply.github.com>
2019-11-21 19:50:50 -0800

    This is a message

---
+++ 11-19/Assets/Scripts/Enemy Scripts/EnemyShooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+
+    public Transform startpos, endpos;
+    public bool spotted;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Raycast();
+    }
+
+    void Raycast()
+    {
+        Debug.DrawLine(startpos.position, endpos.position, Color.red);
+        spotted = Physics2D.Linecast(startpos.position, endpos.position, 1 << LayerMask.NameToLayer("Player"));
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
